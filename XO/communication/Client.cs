362545bfd9de96f7847f036024056290b3561db9
Omer <66028794+omer5144@users.noreply.github.com>
2021-02-01 21:38:45 +0200

    Add files via upload

---
+++ XO/communication/Client.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XO.communication
+{
+    public class Client : Communication
+    {
+        private IPEndPoint remoteEP;
+        private Socket sender;
+        private byte[] bytes;
+        private bool opend;
+
+        public Client(string ip, int port)
+        {
+            bytes = new byte[1024];
+
+
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            this.remoteEP = new IPEndPoint(ipAddress, port);
+            sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+       
+           
+
+            
+
+            opend = false;
+        }
+
+        public void Open()
+        {
+            if (opend)
+                return;
+            sender.Connect(remoteEP);
+            opend = true;
+        }
+
+        public string Send(string msg)
+        {
+            if (!opend)
+                return null;
+
+            sender.Send(Encoding.ASCII.GetBytes(msg));
+            return msg;
+        }
+
+        public string Recv()
+        {
+            if (!opend)
+                return null;
+
+            int bytesRec = sender.Receive(bytes);
+            return Encoding.ASCII.GetString(bytes, 0, bytesRec);
+        }
+
+        public void Close()
+        {
+            if (!opend)
+                return;
+
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+
+            opend = false;
+        }
+    }
+}
---
+++ XO/view/Win.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace XO.view
+{
+    partial class Win
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.winner = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // winner
+            // 
+            this.winner.AutoSize = true;
+            this.winner.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.winner.Location = new System.Drawing.Point(44, 40);
+            this.winner.Name = "winner";
+            this.winner.Size = new System.Drawing.Size(255, 91);
+            this.winner.TabIndex = 0;
+            this.winner.Text = "label1";
+            // 
+            // Win
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(889, 188);
+            this.Controls.Add(this.winner);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Win";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Win";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Win_FormClosed);
+            this.Load += new System.EventHandler(this.Win_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label winner;
+    }
+}
